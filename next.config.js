33b31306c271b362e5af0f877d5b024f743fd8f7
harshit <harshit.gambhir@squareboat.com>
2022-05-12 10:15:04 +0530

    influencers

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ['cullab.s3.ap-south-1.amazonaws.com', 'cullab-dev.s3.ap-south-1.amazonaws.com', '192.168.0.155'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
